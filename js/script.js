bbae53e53599b643be6cc56f55e0705ef1e71d35
Manveet Kaur <90703104+ManveetKaur0007@users.noreply.github.com>
2022-07-01 16:28:33 -0400

    JavaScript and style

--- js/script.js
+++ js/script.js
@@ -7,3 +7,11 @@ Date: 2022/07/01
 Filename: script.js
 Section: 065
 */
+
+$(`.accordion`).on(`click`, `control`, function(e) { 
+    e.preventDefault(); 
+    $(this) 
+        .next(`.panel`) 
+        .not(`:animated`) 
+        .slideToggle(); 
+});
