3a90f9a514d5844a4504596de6b4feb1bd0e7c4b
Manveet Kaur <90703104+ManveetKaur0007@users.noreply.github.com>
2022-07-01 16:36:45 -0400

    index page and java

--- js/script.js
+++ js/script.js
@@ -8,10 +8,23 @@ Filename: script.js
 Section: 065
 */
 
-$(`.accordion`).on(`click`, `control`, function(e) { 
-    e.preventDefault(); 
-    $(this) 
-        .next(`.panel`) 
-        .not(`:animated`) 
-        .slideToggle(); 
-});
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
