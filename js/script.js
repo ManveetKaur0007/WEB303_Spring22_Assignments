e1e1c17544b6fd26709e21f016c6a8a4962c5dbe
Manveet Kaur <90703104+ManveetKaur0007@users.noreply.github.com>
2022-07-03 16:44:55 -0400

    data added to the index file

--- js/script.js
+++ js/script.js
@@ -12,7 +12,6 @@ Section: 065
 /* Accordion JavaScript*/
 var acc = document.getElementsByClassName("accordion");
 var i;
-
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     this.classList.toggle("active");
